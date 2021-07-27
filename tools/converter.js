e8a52b13438b985b67916a393ef709b36376b031
Sander Belen <sander.belen@gmail.com>
2021-07-27 22:21:36 +0200

    setup react, implement tls to json

---
+++ tools/converter.js
@@ -0,0 +1,30 @@
+var fs = require("fs");
+var text = fs.readFileSync("./tools/soatest.tsv").toString("utf-8");
+
+//var csv is the CSV file with headers
+function csvJSON(csv) {
+  var lines = csv.split("\n");
+
+  var result = [];
+
+  var headers = lines[0].split("\t").map((value, index, arr) => {
+    return value.trim();
+  });
+
+  for (var i = 1; i < lines.length; i++) {
+    var obj = {};
+    var currentline = lines[i].split("\t");
+    
+    for (var j = 0; j < headers.length; j++) {
+      obj[headers[j]] = currentline[j].trim();
+    }
+
+    result.push(obj);
+  }
+
+  return JSON.stringify(result, null, 2); //JSON
+}
+
+var json = csvJSON(text);
+
+fs.writeFile("./public/soatest.json", json, "utf8", () => {});
